70cccb181aaa12491667fa209091f8ebe401bcae
Francisco <javiermartincuenca@gmail.com>
2020-12-25 22:47:58 +0100

    Get products from backend

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -9,14 +9,15 @@ namespace Core.Specifications
     public int PageSize
     {
       get => _pageSize;
-      set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+      set => _pageSize = System.Math.Min(value, MaxPageSize);
     }
 
-    public int? BrandId {get; set;}
-    public int? TypeId {get; set;}
+    public int? BrandId { get; set; }
+    public int? TypeId { get; set; }
     public string Sort { get; set; }
     private string _search;
-    public string Search {
+    public string Search
+    {
       get => _search;
       set => _search = value.ToLower();
     }
