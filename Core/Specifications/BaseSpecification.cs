f590b35ce8bd8ef81a7ee0ef471097a1a643a8f6
Francisco <javiermartincuenca@gmail.com>
2020-12-25 14:07:57 +0100

    Added filters by brand and type

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -6,7 +6,7 @@ namespace Core.Specifications
 {
   public class BaseSpecification<T> : ISpecification<T>
   {
-    public BaseSpecification(){}
+    public BaseSpecification() { }
     public BaseSpecification(Expression<Func<T, bool>> criteria)
     {
       Criteria = criteria;
@@ -17,9 +17,23 @@ namespace Core.Specifications
     public List<Expression<Func<T, object>>> Includes { get; } =
         new List<Expression<Func<T, object>>>();
 
+    public Expression<Func<T, object>> OrderBy { get; private set; }
+
+    public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
     protected void AddInclude(Expression<Func<T, object>> includeExpression)
     {
       Includes.Add(includeExpression);
+    }
+
+    protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+      OrderBy = orderByExpression;
+    }
+
+    protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescendingExpression)
+    {
+      OrderByDescending = orderByDescendingExpression;
     }
   }
 }
