7d73b9eaaceb5e738f4f9ab710ef3640d25c70d6
Francisco <javiermartincuenca@gmail.com>
2020-12-25 13:10:43 +0100

    Refactor startup class

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,15 @@
-using System.Linq;
-using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+
 
 namespace API
 {
@@ -27,35 +24,12 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
-      services.AddScoped<IProductRepository, ProductRepository>();
-      services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
       services.AddAutoMapper(typeof(MappingProfiles));
       services.AddControllers();
       services.AddDbContext<StoreContext>(x =>
         x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      });
-
-      services.Configure<ApiBehaviorOptions>(options =>
-      {
-        options.InvalidModelStateResponseFactory = actionContext =>
-        {
-          var errors = actionContext.ModelState
-            .Where(e => e.Value.Errors.Count > 0)
-            .SelectMany(x => x.Value.Errors)
-            .Select(x => x.ErrorMessage).ToArray();
-
-          var errorResponse = new ApiValidationErrorResponse
-          {
-            Errors = errors
-          };
-
-          return new BadRequestObjectResult(errorResponse);
-        };
-      });
+      services.AddApplicationServices();
+      services.AddSwaggerDocumentation();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,8 +39,7 @@ namespace API
 
       if (env.IsDevelopment())
       {
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1"));
+        app.UseSwaggerDocumentation();
       }
 
       app.UseStatusCodePagesWithReExecute("/errors/{0}");
