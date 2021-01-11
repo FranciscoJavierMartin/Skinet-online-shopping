cb913101f5cb47c6f4f4d2a22707caf39c97c7b2
Francisco <javiermartincuenca@gmail.com>
2021-01-11 15:26:38 +0100

    Cache on backend

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -23,6 +23,7 @@ namespace API.Controllers
       _unitOfWork = unitOfWork;
     }
 
+    [Cached(600)]
     [HttpGet]
     public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
       [FromQuery] ProductSpecParams productParams)
@@ -39,6 +40,7 @@ namespace API.Controllers
         productParams.PageSize, totalItems, data));
     }
 
+    [Cached(600)]
     [HttpGet("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
@@ -50,12 +52,14 @@ namespace API.Controllers
         : NotFound(new ApiResponse(404));
     }
 
+    [Cached(600)]
     [HttpGet("brands")]
     public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
     {
       return Ok(await _unitOfWork.Repository<ProductBrand>().ListAllAsync());
     }
 
+    [Cached(600)]
     [HttpGet("types")]
     public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
     {
